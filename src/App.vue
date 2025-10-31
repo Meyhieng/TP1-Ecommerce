bba6883c7f7480980ccc4bf241a577d4d25cd906
Meyhieng <sanmeyhieng22@gmail.com>
2025-10-31 13:50:54 +0700

    Add component

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,62 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+
+  import { ref } from 'vue'
+  import Promotion from './components/promotion.vue'
+  import Category from './components/category.vue'
+
+
+
+  const catDatas = ref([
+    { imgScr: "/CategoriesItem/Cat1.png", catName: "Cake & Milk", quantity: 14, cardColor: "#F2FCE4" },
+    { imgScr: "/CategoriesItem/Cat2.png", catName: "Peach", quantity: 17, cardColor: "#FFFCEB" },
+    { imgScr: "/CategoriesItem/Cat3.png", catName: "Organic Kiwi", quantity: 21, cardColor: "#ECFFEC" },
+    { imgScr: "/CategoriesItem/Cat4.png", catName: "Red Apple", quantity: 68, cardColor: "#FEEFEA" },
+    { imgScr: "/CategoriesItem/Cat5.png", catName: "Snack", quantity: 34, cardColor: "#FFF3EB" },
+    { imgScr: "/CategoriesItem/Cat6.png", catName: "Black plum", quantity: 25, cardColor: "#FFF3FF" },
+    { imgScr: "/CategoriesItem/Cat7.png", catName: "Vegetables", quantity: 65, cardColor: "#F2FCE4" },
+    { imgScr: "/CategoriesItem/Cat8.png", catName: "Headphone", quantity: 33, cardColor: "#FFFCEB" },
+    { imgScr: "/CategoriesItem/Cat9.png", catName: "Cake & Milk", quantity: 54, cardColor: "#F2FCE4" },
+    { imgScr: "/CategoriesItem/Cat10.png", catName: "Orange", quantity: 63, cardColor: "#FFF3FF" }
+  ])
+
+  const promoDatas = ref([
+    { promoText: "Everyday Fresh & Clean with Our Products", promoImg: "/Promotion/Pro1.jpg", promoColor: "#F0E8D5", btnColor: "#3BB77E" },
+    { promoText: "Make your Breakfast Healthy and Easy", promoImg: "/Promotion/Pro2.png", promoColor: "#F3E8E8", btnColor: "#3BB77E" },
+    { promoText: "The best Organic Products Online", promoImg: "/Promotion/Pro3.jpg", promoColor: "#E7EAF3", btnColor: "#FDC040" }
+  ])
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="category">
+    <category
+      v-for="(cat, index) in catDatas"
+      :key="index"
+      class="box"
+      :imgScr="cat.imgScr"
+      :catName="cat.catName"
+      :quantity="cat.quantity"
+      :cardColor="cat.cardColor"
+    />
+  </div>
 
-    <div class="wrapper">
-      <!-- <HelloWorld msg="You did it!" /> -->
-
-      <nav>
-        <!-- <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink> -->
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="promotion">
+    <promotion
+      v-for="(promo, index) in promoDatas"
+      :key="index"
+      class="box"
+      :promoText="promo.promoText"
+      :promoImg="promo.promoImg"
+      :promoColor="promo.promoColor"
+      :btnColor="promo.btnColor"
+    />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+<style scoped> 
+  .category, .promotion { 
+    display: flex; 
+    flex-wrap: wrap; 
+    justify-content: center; 
+    gap: 10px; padding: 10px; 
+    } 
 </style>
