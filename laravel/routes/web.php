34c2996beeed92e5f0e2bc98b1b118361a9eca11
Meyhieng <sanmeyhieng22@gmail.com>
2025-12-27 10:38:53 +0700

    Update TP05

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/students', [StudentController::class, 'index']);
+    Route::get('/students/create', [StudentController::class, 'create']);
+    Route::post('/students', [StudentController::class, 'store']);
+    Route::get('/students/{id}/edit', [StudentController::class, 'edit']);
+    Route::put('/students/{id}', [StudentController::class, 'update']);
+    Route::delete('/students/{id}', [StudentController::class, 'destroy']);
 });
 
 require __DIR__.'/auth.php';
+
+
