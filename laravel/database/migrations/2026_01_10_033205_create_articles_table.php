d4c98ab083c18e6ef21326908272e4a6a82f6db3
Meyhieng <sanmeyhieng22@gmail.com>
2026-01-10 10:52:58 +0700

    add TP07

---
+++ laravel/database/migrations/2026_01_10_033205_create_articles_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('author_id')->constrained()->onDelete('cascade'); // Link to authors
+            $table->string('title');
+            $table->text('content')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
+
